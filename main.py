1d12f56c742e83dd2e6b402ce0e3fade13f9041a
Tom <tom@tomk.ca>
2023-08-03 10:58:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from binance.um_futures import UMFutures
 from binance.lib.utils import config_logging
 import logging
 
-api_key = 'fa04b7d881a6d9926f7afcc7c9890791b4a171e9016825f2525df7ebd54f54c9'
-api_secret = '0fffbecdc98fb483d485c13bfbf8531e34b5bf3e5d8f86b45f14921affe44c08'
+live_api_key = 'yR7thAjvm3ew1dSp4eaUTlVD3CyArKKPjp95kAXW6NQmIYlSuO3SwVOU9OA5Ca4i'
+live_api_secret = '5thfP47BW2udjxQcrEFop2RjSBzKGc71qdz43UrxOSW5Nilu8zVnpSTRPyxMT1ca'
 
-client = UMFutures(api_key, api_secret, base_url='https://testnet.binancefuture.com')
+test_api_key = 'fa04b7d881a6d9926f7afcc7c9890791b4a171e9016825f2525df7ebd54f54c9'
+test_api_secret = '0fffbecdc98fb483d485c13bfbf8531e34b5bf3e5d8f86b45f14921affe44c08'
+
+live_client = UMFutures(live_api_key, live_api_secret)
+test_client = UMFutures(test_api_key, test_api_secret, base_url='https://testnet.binancefuture.com')
 
 app = Flask(__name__)
 
@@ -19,6 +23,9 @@ def webhook():
     action = data.get('action')
     symbol = data.get('ticker')
     trade_amount = float(data.get('trade_amount', '0.001'))  # Convert to float and default to 0.001 if 'trade_amount' is not in data
+    account_type = data.get('account_type', 'test')  # Default to 'test' if 'account_type' is not in data
+
+    client = live_client if account_type == 'live' else test_client
 
     # Fetch current position
     current_positions = client.get_position_risk(symbol=symbol)
@@ -54,4 +61,3 @@ def webhook():
 
 if __name__ == "__main__":
     app.run(port=5000)
-
