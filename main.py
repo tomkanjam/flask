bff88bf2216cfa6cc5c00720da7479741a666d41
Tom <tom@tomk.ca>
2023-08-17 16:58:57 -0600

    Error upgrade and conversion to single order

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from binance.um_futures import UMFutures
 from binance.lib.utils import config_logging
 import logging
+from binance.error import ClientError
 
 live_api_key = 'yR7thAjvm3ew1dSp4eaUTlVD3CyArKKPjp95kAXW6NQmIYlSuO3SwVOU9OA5Ca4i'
 live_api_secret = '5thfP47BW2udjxQcrEFop2RjSBzKGc71qdz43UrxOSW5Nilu8zVnpSTRPyxMT1ca'
@@ -26,13 +27,12 @@ def uptime9164():
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.get_json()
-    # print(data)
     logging.info(f'Received alert: {data}')
 
     action = data.get('action')
     symbol = data.get('ticker')
-    trade_amount = float(data.get('trade_amount', '0.001'))  # Convert to float and default to 0.001 if 'trade_amount' is not in data
-    account_type = data.get('account_type', 'test')  # Default to 'test' if 'account_type' is not in data
+    trade_amount = float(data.get('trade_amount', '0.001'))
+    account_type = data.get('account_type', 'test')
 
     client = live_client if account_type == 'live' else test_client
 
@@ -46,27 +46,38 @@ def webhook():
         if position['symbol'] == symbol:
             current_position = position
             break
-    
+
+    order_amt_to_close_position = 0
     if current_position:
         # Calculate order amount to close position
         order_amt_to_close_position = float(current_position['positionAmt'])
 
-        if order_amt_to_close_position != 0:
-            # Create a new market order in opposite direction to close position
-            order_side = "BUY" if order_amt_to_close_position < 0 else "SELL"
-            client.new_order(symbol=symbol, side=order_side, 
-                         type='MARKET',
-                         quantity=abs(order_amt_to_close_position))
+    # Determine if the existing position direction is opposite to the new order direction
+    existing_position_opposite = (order_amt_to_close_position < 0 and action == 'buy') or (order_amt_to_close_position > 0 and action == 'sell')
+
+    # Calculate the net order amount
+    if existing_position_opposite:
+        net_order_amount = trade_amount + abs(order_amt_to_close_position)
+    else:
+        net_order_amount = max(0, trade_amount - abs(order_amt_to_close_position))
 
     # Open new market order
     order_side = "BUY" if action == 'buy' else "SELL"
-    client.new_order(symbol=symbol, side=order_side, type='MARKET', 
-                 quantity=trade_amount)
+    try:
+        client.new_order(symbol=symbol, side=order_side, type='MARKET', quantity=net_order_amount)
+    except ClientError as e:
+        # Log the error along with the symbol that caused it
+        logging.error(f"Error when placing order for symbol '{symbol}': {e}")
+        return {
+            "code": "error",
+            "message": f"Failed to place order for symbol '{symbol}': {e}"
+        }, 500
 
     return {
         "code": "success",
         "message": "webhook received"
     }, 200
+    
 
 if __name__ == "__main__":
     app.run(port=5000)
